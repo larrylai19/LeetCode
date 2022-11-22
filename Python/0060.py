3661c42e6623348d1115d686b2e413405f8cd376
larrylai19 <stji9gjz2@gmail.com>
2022-11-22 15:03:24 +0800

    Create 0060.py

---
+++ Python/0060.py
@@ -0,0 +1,18 @@
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        result = ''
+        numbers = [str(i) for i in range(1, 11)]
+        
+        factorial = [1] * n
+        for i in range(1, len(factorial)):
+            factorial[i] = i * factorial[i - 1]
+        
+        # for index
+        k -= 1
+        for i in range(n, 0, -1):
+            j = k // factorial[i - 1]
+            k %= factorial[i - 1]
+            result += numbers[j]
+            del numbers[j]
+
+        return result
